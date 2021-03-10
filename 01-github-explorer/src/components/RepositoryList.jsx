1012a04ea8380d9f5c1507cb31ab317abdc0189a
felipemaxrosa <felipemax.suporteti@gmail.com>
2021-03-10 16:19:15 -0300

    classes 01.01 until 02.01

---
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de Repositórios</h1>
+
+      <ul>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in Reactjs</p>
+          <a href="">Acessar repositório</a>
+        </li>
+      </ul>
+    </section>
+  )
+}
